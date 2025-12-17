13764a988389f9492da468289b7c68e3b692db3f
[honza] <[srajbis@gmail.com]>
2025-12-17 20:59:15 +0100

    deleted unused method

--- src/body.py
+++ src/body.py
@@ -23,23 +23,6 @@ class Body:
         force = distance.normalize() * force_magnitude
         self.force += force
 
-    def update(self, dt):
-        """
-        Velocity Verlet integrace:
-        r(t+dt) = r(t) + v(t)*dt + 0.5*a(t)*dt^2
-        v(t+dt) = v(t) + 0.5*(a(t) + a(t+dt))*dt
-        """
-        # předpokládáme, že self.force je již spočítána
-        new_acceleration = self.force / self.mass
-        # aktualizace pozice
-        self.position += self.velocity * dt + self.acceleration * (0.5 * dt**2)
-        # dočasná proměnná pro starou akceleraci
-        old_acceleration = self.acceleration
-        # nová akcelerace se uloží po výpočtu sil znovu v simulaci
-        self.acceleration = new_acceleration
-        # aktualizace rychlosti (budeme potřebovat novou sílu po compute_forces)
-        self.velocity += old_acceleration * (0.5 * dt)  # částečný update, dokončeno po recompute_forces
-
     def reset_force(self):
         self.force = Vector(0.0, 0.0)
     
